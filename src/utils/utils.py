76fe28be288fe7e47643f68dca90d61e1d81d72b
Robert <robert.adragna1381@gmail.com>
2025-09-15 13:43:02 +0100

    Added common utility function to render prompts across tasks

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,3 +1,25 @@
+from jinja2 import Environment, FileSystemLoader
+from typing import List
+
+
 REPO_ROOT = "/Users/RobertAdragna/Documents/MATS/evals_suite_new"
 
 TRASH_LOGS_DIR = f"{REPO_ROOT}/tests/trash_logs"
+
+
+
+
+class PromptRenderer:
+    def __init__(self, dataset_prompt_dir, experiment_prompt_dir):
+        self.dataset_prompt_dir = dataset_prompt_dir
+        self.experiment_prompt_dir = experiment_prompt_dir
+
+    def render_jinja_template(self, template_name: str, root_paths: List[str] = [], **kwargs) -> str:
+        """Render a Jinja template with the given context"""
+
+        env = Environment(loader=FileSystemLoader(root_paths + [self.dataset_prompt_dir, self.experiment_prompt_dir]))
+
+        template = env.get_template(template_name)
+        rendered_prompt = template.render(**kwargs)
+
+        return rendered_prompt
